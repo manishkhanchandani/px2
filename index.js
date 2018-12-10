d587d142f91eb44b825ad57f7b6a2ad6ee3f29c5
Manish Khanchandani <manishkk74@gmail.com>
2018-12-11 00:49:58 +0530

    removing unnecessary files

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
 var request = require('request');
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
 
 app.get('/search.xml', function(req, res){
 							console.log('q: ', req.query);
